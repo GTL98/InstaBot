197ff707effb962294b1b60a7e91bdb612cd6053
Guilherme Trevisan Linhares <91161693+GTL98@users.noreply.github.com>
2022-05-30 12:28:41 -0300

    Arquivo main.py
    Arquivo destinado a executar o código do InstaBot.

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 # Importar os módulos do InstaBot
 from deixar_seguir import DeixarSeguir
 from curtir_hashtags import CurtirHashtags
+from curtir_postagens_perfil import CurtirPerfil
 
 # Informar qual ação o InstaBot fará
-acao = 0
+acao = 1
 
 # Informar login, senha e hashtag
 login = ''
 senha = ''
+perfil = ''
 hashtag = ''
+quantidade_curtir = 0
 
 if __name__ == '__main__':
-    if acao == 0:
+    if acao == 1:
         DeixarSeguir(login, senha)
-    elif acao == 1:
-        CurtirHashtags(login, senha, hashtag)
+    elif acao == 2:
+        CurtirHashtags(login, senha, hashtag, quantidade_curtir)
+    elif acao == 3:
+        CurtirPerfil(login, senha, perfil, quantidade_curtir)
